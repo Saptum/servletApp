7a6900003c83b47494b435e857b782fc41543b42
Your Name <you@example.com>
2021-12-09 15:43:11 +0200

    something

--- src/main/java/com/example/demo/session/LoginDataStructure.java
+++ src/main/java/com/example/demo/session/LoginDataStructure.java
@@ -6,9 +6,8 @@ import java.util.List;
 
 
 public class LoginDataStructure {
-
     public static HashMap<String, String> mapUsers = new HashMap<>();
-
+    public static List<String[]> listUsers = new ArrayList<>();
 
     public static void initMapUsers() {
         mapUsers.put("Sam", "241424");
@@ -18,6 +17,14 @@ public class LoginDataStructure {
         mapUsers.put("Ezio", "creed");
     }
 
+    public static void initList(){
+        listUsers.add(new String[]{"Sam", "241424"});
+        listUsers.add(new String[]{"Din", "super"});
+        listUsers.add(new String[]{"Harry", "423rvq32"});
+        listUsers.add(new String[]{"Tom", "353q514"});
+        listUsers.add(new String[]{"Ezio", "creed"});
+    }
+
     public static boolean checkInitMap(String login, String password) {
         if (mapUsers.containsKey(login) && mapUsers.get(login).equals(password)) {
             return true;
@@ -25,4 +32,13 @@ public class LoginDataStructure {
         return false;
     }
 
+    public static boolean checkInitList(String login, String password){
+        for (String[] x:listUsers
+        ) {
+            if (x[0].equals(login) && x[1].equals(password)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
