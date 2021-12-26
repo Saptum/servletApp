dc4ccf05e861b3b058634c9df1d7ac5c5faef184
Your Name <you@example.com>
2021-12-26 14:33:35 +0200

    add new class ViewProductsServlet

---
+++ src/main/java/com/example/demo/ViewProductsServlet.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+@WebServlet("/viewProductsServlet")
+public class ViewProductsServlet  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        try {
+            EmployeeRepository.showProducts(out);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
