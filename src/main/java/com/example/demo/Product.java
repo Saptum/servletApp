f3a8e57fef9afab90158e7f59c952cef3a569625
Your Name <you@example.com>
2021-12-26 14:33:05 +0200

    add new class Product

---
+++ src/main/java/com/example/demo/Product.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+public class Product {
+    private String name;
+    private int id;
+    private int price;
+    private String description = "";
+
+    public Product(String name, int id, int price, String description) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
