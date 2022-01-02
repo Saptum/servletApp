016816cff968f0188cc09cec8e80976212afb306
Your Name <you@example.com>
2022-01-02 17:04:55 +0200

    Revert "something"
    This reverts commit e518ed58ca771bc671ab7fbfd2982ff22f509509.

--- src/main/java/com/example/demo/filters/AuthenticationFilter.java
+++ src/main/java/com/example/demo/filters/AuthenticationFilter.java
@@ -29,37 +29,21 @@ public class AuthenticationFilter implements Filter {
 
         HttpSession session = req.getSession(false);
 
-//        if (session == null){
-//            if (AuthentificationCheck.checkLogin(uri)){
-//                chain.doFilter(request, response);
-//            }else {
-//                this.servletContext.log("<<< Unauthorized access request");
-//                PrintWriter out = res.getWriter();
-//                out.println("No access because you have not logged in!!!");
-//            }
-//       }else if (!AuthentificationCheck.checkListUri(uri)) {
-//            this.servletContext.log("<<< Unauthorized access request");
-//            PrintWriter out = res.getWriter();
-//            out.println("You have no rights to do that!!!");
-//        } else {
-//            chain.doFilter(request, response);
-//       }
-//    }
-
- if (session == null){
-        if (AuthentificationCheck.checkLogin(uri)){
-            chain.doFilter(request, response);
-        }else {
-            this.context.log("<<< Unauthorized access request");
+        if (session == null){
+            if (AuthentificationCheck.checkLogin(uri)){
+                chain.doFilter(request, response);
+            }else {
+                this.servletContext.log("<<< Unauthorized access request");
+                PrintWriter out = res.getWriter();
+                out.println("No access because you have not logged in!!!");
+            }
+       }else if (!AuthentificationCheck.checkListUri(uri)) {
+            this.servletContext.log("<<< Unauthorized access request");
             PrintWriter out = res.getWriter();
-            out.println("No access because you have not logged in!!!");
-        }
-    }else if (!AuthentificationCheck.checkMapUri(uri)) {
-        this.context.log("<<< Unauthorized access request");
-        PrintWriter out = res.getWriter();
-        out.println("You have no rights to do that!!!");
-    } else {
-        chain.doFilter(request, response);
+            out.println("You have no rights to do that!!!");
+        } else {
+            chain.doFilter(request, response);
+       }
     }
 
     public void destroy() {
